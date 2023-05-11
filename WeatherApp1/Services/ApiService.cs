bc93bff05badfcbc9165d2ab0ab24e3cc1ecb065
user <user@ASPIRE5>
2023-05-11 05:34:22 +0300

    Add project files.

---
+++ WeatherApp1/Services/ApiService.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp1.Models;
+
+namespace WeatherApp1.Services
+{
+    public static class ApiService
+    {
+        public static async Task<Root> GetWeather(double latidute, double longitude ) { 
+           var httpClient = new HttpClient();
+           var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&units=metric&appid=91d3467e7d6a21edb46e31643c4a08ce", latidute, longitude));
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+
+        public static async Task<Root> GetWeatherByCity(string city)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/forecast?q={0}&units=metric&appid=91d3467e7d6a21edb46e31643c4a08ce", city));
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+
+    }
+}
---
+++ WeatherApp1/WeatherPage.xaml.cs
@@ -0,0 +1,71 @@
+using WeatherApp1.Models;
+using WeatherApp1.Services;
+
+namespace WeatherApp1;
+
+public partial class WeatherPage : ContentPage
+{
+    public List<Models.List> WeatherList;
+    private double latitude;
+    private double longitude;
+ 
+	public WeatherPage()
+	{
+        InitializeComponent();
+        WeatherList = new List<Models.List>();
+	}
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        await GetLocation();
+        await GetWeatherDataByLocation(latitude, longitude);
+      
+    }  
+
+    public async Task GetLocation() {
+       var location = await Geolocation.GetLocationAsync();
+        latitude = location.Latitude;
+        longitude = location.Longitude;
+    }
+
+    private async void TapLoacation_Tapped(object sender, EventArgs e) {
+        await GetLocation();
+        await GetWeatherDataByLocation(latitude, longitude);
+    
+    }
+
+    public async Task GetWeatherDataByLocation(double latitude, double longitude) {
+            var result = await ApiService.GetWeather(latitude, longitude);
+            UpdateUI(result);     
+        }
+
+    private async void ImageButton_Clicked(object sender, EventArgs e) {
+        var response = await DisplayPromptAsync(title:"", message:"", placeholder:"Search weather by city", accept:"Search", cancel:"Cancel");
+        if (response != null) {
+            await GetWeatherDataByCity(response);
+        }
+    }
+
+    public async Task GetWeatherDataByCity(string city)
+    {
+        var result = await ApiService.GetWeatherByCity(city);
+        UpdateUI(result);
+    }
+
+    public void UpdateUI(Root result) {
+        foreach (var item in result.list)
+        {
+            WeatherList.Add(item);
+        }
+
+        CvWeather.ItemsSource = WeatherList;
+
+        LblCity.Text = result.city.name;
+        LblWeatherDescription.Text = result.list[0].weather[0].description;
+        LblTemperature.Text = result.list[0].main.temperature + "°C";
+        LblHumidity.Text = result.list[0].main.humidity + "%";
+        LblWind.Text = result.list[0].wind.speed + "km/h";
+        ImageWeatherIcon.Source = result.list[0].weather[0].customIcon;
+    }
+}
